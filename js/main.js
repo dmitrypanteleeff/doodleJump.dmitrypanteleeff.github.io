6e1ce9248fc269a76c8206bf3262c27a1ee5bc8c
Dmitry <DmitryPanteleeff@yandex.ru>
2020-11-16 16:44:11 +0300

    поставил функцию свайпа в игру

--- js/main.js
+++ js/main.js
@@ -242,54 +242,56 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 }*/
     function controlMob(e){
-        document.addEventListener('click',console.log("Текст"));
-        var startPoint = {};
-        var nowPoint;
-        var ldelay;
-        document.addEventListener('touchstart', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            startPoint.x = event.changedTouches[0].pageX;
-            startPoint.y = event.changedTouches[0].pageY;
-            ldelay = new Date();
-            gridTest.innerHTML = "Тык ";
-        }, false);
-        /*Ловим движение пальцем*/
-        document.addEventListener('touchmove', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var otk = {};
-            nowPoint = event.changedTouches[0];
-            otk.x = nowPoint.pageX - startPoint.x;
-            /*Обработайте данные*/
-            /*Для примера*/
-            if (Math.abs(otk.x) > 200) {
-                if (otk.x < 0) { gridTest.innerHTML = "свайп влево";/*СВАЙП ВЛЕВО(ПРЕД.СТРАНИЦА)*/ }
-                if (otk.x > 0) { gridTest.innerHTML = "свайп вправо";/*СВАЙП ВПРАВО(СЛЕД.СТРАНИЦА)*/ }
-                startPoint = { x: nowPoint.pageX, y: nowPoint.pageY };
-            }
-        }, false);
-        /*Ловим отпускание пальца*/
-        document.addEventListener('touchend', function (event) {
-            var pdelay = new Date();
-            nowPoint = event.changedTouches[0];
-            var xAbs = Math.abs(startPoint.x - nowPoint.pageX);
-            var yAbs = Math.abs(startPoint.y - nowPoint.pageY);
-            if ((xAbs > 20 || yAbs > 20) && (pdelay.getTime() - ldelay.getTime()) < 200) {
-                if (xAbs > yAbs) {
-                    if (nowPoint.pageX < startPoint.x) {gridTest.innerHTML = "свайп влево";/*СВАЙП ВЛЕВО*/ }
-                    else {gridTest.innerHTML = "свайп вправо";/*СВАЙП ВПРАВО*/ }
+        if (!isGameOver){
+            document.addEventListener('click',console.log("Текст"));
+            var startPoint = {};
+            var nowPoint;
+            var ldelay;
+            document.addEventListener('touchstart', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                startPoint.x = event.changedTouches[0].pageX;
+                startPoint.y = event.changedTouches[0].pageY;
+                ldelay = new Date();
+                gridTest.innerHTML = "Тык ";
+            }, false);
+            /*Ловим движение пальцем*/
+            document.addEventListener('touchmove', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                var otk = {};
+                nowPoint = event.changedTouches[0];
+                otk.x = nowPoint.pageX - startPoint.x;
+                /*Обработайте данные*/
+                /*Для примера*/
+                if (Math.abs(otk.x) > 100) {
+                    if (otk.x < 0) { gridTest.innerHTML = "свайп влево"; moveLeft();/*СВАЙП ВЛЕВО(ПРЕД.СТРАНИЦА)*/ }
+                    if (otk.x > 0) { gridTest.innerHTML = "свайп вправо"; moveRight();/*СВАЙП ВПРАВО(СЛЕД.СТРАНИЦА)*/ }
+                    startPoint = { x: nowPoint.pageX, y: nowPoint.pageY };
                 }
-                else {
-                    if (nowPoint.pageY < startPoint.y) {/*СВАЙП ВВЕРХ*/ }
-                    else {/*СВАЙП ВНИЗ*/ }
+            }, false);
+            /*Ловим отпускание пальца*/
+            document.addEventListener('touchend', function (event) {
+                var pdelay = new Date();
+                nowPoint = event.changedTouches[0];
+                var xAbs = Math.abs(startPoint.x - nowPoint.pageX);
+                var yAbs = Math.abs(startPoint.y - nowPoint.pageY);
+                if ((xAbs > 20 || yAbs > 20) && (pdelay.getTime() - ldelay.getTime()) < 200) {
+                    if (xAbs > yAbs) {
+                        if (nowPoint.pageX < startPoint.x) {gridTest.innerHTML = "свайп влево"; moveLeft();/*СВАЙП ВЛЕВО*/ }
+                        else {gridTest.innerHTML = "свайп вправо"; moveRight();/*СВАЙП ВПРАВО*/ }
+                    }
+                    else {
+                        if (nowPoint.pageY < startPoint.y) {gridTest.innerHTML = "свайп вверх"; moveStraight();/*СВАЙП ВВЕРХ*/ }
+                        else {/*СВАЙП ВНИЗ*/ }
+                    }
                 }
-            }
-        }, false);
+            }, false);
+        }
     }
 
 
-    
+
     function control(e){
         
         //console.log(e);
